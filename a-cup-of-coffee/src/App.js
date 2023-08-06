6d8a3b6ddda8eff07b07b6d142221037fa4da1ec
hurmong <hurmong@hanmail.net>
2023-08-07 04:55:56 +0900

    [Refactor]App.js 수정

--- a-cup-of-coffee/src/App.js
+++ a-cup-of-coffee/src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import CategoryPage from "./js/pages/categoryPage";
+import MainPage from "./js/pages/MainPage";
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
       <Routes>
+        <Route path="main" element={MainPage()}/>
         <Route path="category" element={CategoryPage()} />
       </Routes>
     </Fragment>
